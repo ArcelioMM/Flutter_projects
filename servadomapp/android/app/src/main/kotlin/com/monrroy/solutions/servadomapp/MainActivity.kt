650c575aff38dc15c54330adf29dc05fd816b359
ArcelioMM114 <ingamm62@ArcelioMM114s-MacBook-Pro.local>
2026-03-25 04:42:56 -0700

    COMMIT CON CASI TODOS LOS PUBS NECESARIOS

---
+++ servadomapp/android/app/src/main/kotlin/com/monrroy/solutions/servadomapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.monrroy.solutions.servadomapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ servadomapp/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:servadomapp/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Inicializa Firebase
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
