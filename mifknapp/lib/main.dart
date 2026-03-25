86380f67150b5d781bbf55e2e78a780d7b86f7cb
ArcelioMM114 <ingamm62@ArcelioMM114s-MacBook-Pro.local>
2026-03-24 20:40:06 -0700

    Ya estamos readyyy hostiaaa brrrrrr

--- mifknapp/lib/main.dart
+++ mifknapp/lib/main.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 
-String confMac = "?";
-
+String confMac = "YA EH CONFIRMADO TIO";
 
 void main() {
   runApp(const MyApp());
@@ -15,15 +13,12 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SnackBar Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
+      theme: ThemeData(primarySwatch: Colors.indigo),
       home: const HomePage(),
     );
   }
@@ -35,30 +30,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SnackBar Confirma'),
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: const Text('SnackBar Confirma'), centerTitle: true),
       body: Center(
-        
         child: Column(
           children: [
-          Text("EL FOCKIN CHELO"),
-          Text("Ahora si estamos activo, \nconfirmao desde Windows10!\nconfirmao desde MacOS"+confMac+""),
-          if (confMac == "?")
+            Text("EL FOCKIN CHELO"),
+            SizedBox(height: 30,),
+            if(confMac=="win")
+            Text("Confirmado desde macOS y windOWS"),
+            if(confMac!="win")
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.indigo,
-                padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 12,
+                ),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
               ),
               onPressed: () {
-                confMac="!\n      Izzzzzzz Brrrr Brrrrr";
                 final snackBar = SnackBar(
-                  content: const Text(
-                    '¡Hola! Este es un SnackBar estilizado 😎',
+                  content: Text(
+                    confMac + "😎",
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
                   ),
                   backgroundColor: Colors.indigo.shade400,
@@ -79,11 +74,10 @@ class HomePage extends StatelessWidget {
                 ScaffoldMessenger.of(context).showSnackBar(snackBar);
               },
               child: const Text(
-                "CONFIRMA MAC!!",
+                "CONFIRMO MAC??",
                 style: TextStyle(fontSize: 18, color: Colors.white),
               ),
             ),
-          
           ],
         ),
       ),
